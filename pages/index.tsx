16d1c455a6bcb87d61fcca9a1503e6579f767101
liaoyl <liaoyl@duneproject.xyz>
2022-11-22 10:37:52 +0800

    init

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import * as d3 from "d3"
+import { useEffect } from "react"
+
+export default function Home() {
+  useEffect(() => {
+    const body = d3.select("body")
+    console.log({ body, window: d3.select(window) })
+    console.log(`select body is correct in both cases`)
+    console.log(`select window is correct in turbo mode`) //
+  }, [])
+
+  return <div>home</div>
+}
